02237275134938af625cf730ab1488cdd7ccf975
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-03-31 10:39:02 +0530

    initial commit

---
+++ frontend/src/Create.jsx
@@ -0,0 +1,7 @@
+export default function Create(){
+    return (
+        <div>
+            <input type="text" placeholder="Enter Name" />
+        </div>
+    )
+}
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,10 @@
+import Create from "./Create";
+
+export default function Home(){
+    return(
+        <div className="home">
+<h1>Contact Link</h1> 
+<Create/>   
+       </div>
+    )
+}
