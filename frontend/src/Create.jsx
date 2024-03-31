1fa440b8b9afcc7d7ca18ddb31f84810a6a1c04f
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-03-31 11:21:52 +0530

    button

--- frontend/src/Create.jsx
+++ frontend/src/Create.jsx
@@ -1,7 +1,16 @@
 export default function Create(){
     return (
+        <>
         <div className="create_form">
-            <input type="text" placeholder="Enter Name" />
+            <input type="text" placeholder="Name" />
+            <input type="number" placeholder="Phone" />
+            <input type="email" placeholder="Email" />
+            </div>
+
+        <div className="btn">
+              <button ><b>Save</b></button>
+              <button ><b>View</b></button>
         </div>
+</>
     )
 }
