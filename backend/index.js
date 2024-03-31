2dc65bc6b899c48afd808f9d017f5a37b6dab51f
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-03-31 14:50:49 +0530

    db connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
-const express = require('express');
+const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const ContactModel = require('./Models/contact')
 
-const app = express;
+const app = express()
 app.use(cors())
 app.use(express.json())
 
 mongoose.connect('mongodb://localhost:27017/test')
+
+app.post('/add',(req,res)=>{
+    const {name,phone,email} = req.body;
+    ContactModel.create({
+        name:name,
+        phone:phone,
+        email:email
+    }).then(result=>{
+        res.json(result);
+    }).catch(err=>{
+        res.json(err);
+    });
+})
+app.listen(3001,()=>{
+    console.log("server is running")
+})
--- frontend/src/Create.jsx
+++ frontend/src/Create.jsx
@@ -1,14 +1,28 @@
+import { useState } from 'react'
+import axios from 'axios'
 export default function Create(){
+    const [name,setName] = useState('');
+    const [phone,setPhone] = useState('');
+    const [email,setEmail] = useState('');
+
+    const handleSubmit = () =>{
+        axios.post('http://localhost:3001/add',{name,phone,email});
+        setName('');
+        setPhone('');
+        setEmail('');
+        alert("Contact saved successfully");
+    }
+
     return (
         <>
         <div className="create_form">
-            <input type="text" placeholder="Name" />
-            <input type="number" placeholder="Phone" />
-            <input type="email" placeholder="Email" />
+            <input type="text" placeholder="Name" onChange={(e)=> setName(e.target.value)} />
+            <input type="number" placeholder="Phone" onChange={(e)=> setPhone(e.target.value)}/>
+            <input type="email" placeholder="Email" onChange={(e)=> setEmail(e.target.value)} />
             </div>
 
         <div className="btn">
-              <button ><b>Save</b></button>
+              <button onClick={handleSubmit}><b>Save</b></button>
               <button ><b>View</b></button>
         </div>
 </>
